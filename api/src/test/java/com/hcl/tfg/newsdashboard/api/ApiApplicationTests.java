847ca864830a79ce8175e80f4c7213f1b6dbf915
Balavijay Duraibabu <bala.vijay@hcl.com>
2020-03-12 09:30:44 +0530

    Add modules

---
+++ api/src/test/java/com/hcl/tfg/newsdashboard/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.tfg.newsdashboard.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
